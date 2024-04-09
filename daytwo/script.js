a1df2d6dca53935e59e1f3c828c62ca7ce85383a
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-04-09 12:08:09 +0545

    added day two

---
+++ daytwo/script.js
@@ -0,0 +1,4 @@
+let word1 = "B";
+let word2 = "A";
+let result = word1 < word2;
+console.log(result);
---
+++ daytwo/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+// assignment operator
+// let a = 90;
+
+// console.log(a);
+
+// addition
+let a;
+b = 40;
+
+console.log(a + b);
+a = 4;
